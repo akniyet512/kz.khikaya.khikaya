9908a6f2ca360aa5bb04415c2d322284e9e09db7
Dilfina <190103057@stu.sdu.edu.kz>
2023-03-05 14:00:30 +0600

    Event page has some changes

--- lib/screens/mentor_account/first_event_screen.dart
+++ lib/screens/mentor_account/first_event_screen.dart
@@ -15,24 +15,30 @@ class _FirstEventScreenState extends State<FirstEventScreen> {
     Size screenSize = Utils().getScreenSize();
     return Scaffold(
       appBar: AppBarWidgets(screenSize: screenSize),
-      body: Column(
-        children: [
-          GestureDetector(
-            onTap: () {},
-            child: const Text(
-              "< back",
-              style: TextStyle(
-                  color: Color(0xFF787878),
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: screenSize.width / 12),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Text(
+                  "< back",
+                  style: TextStyle(
+                      color: Color(0xFF787878),
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15),
+                ),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          EventWidget()
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            EventWidget()
+          ],
+        ),
       ),
     );
   }
@@ -68,7 +74,7 @@ class _EventWidgetState extends State<EventWidget> {
     return Center(
       child: Container(
         width: screenSize.width / 1.2,
-        height: screenSize.height / 1.35,
+        //height: screenSize.height / 1.35,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(7),
           border: Border.all(width: 1, color: Color(0xFF000000)),
@@ -133,33 +139,34 @@ class _EventWidgetState extends State<EventWidget> {
               color: Colors.grey,
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 30.0),
+              padding: const EdgeInsets.only(left: 30.0, top: 35),
               child: Column(
                 //mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   TextFieldWidget(
                     maxLines: 1,
-                    height: 40,
+                    height: 38,
                     title: "Event Name*",
                     controller: nameController,
                   ),
                   TextFieldWidget(
                     maxLines: 1,
-                    height: 40,
+                    height: 38,
                     title: "Location",
                     controller: locationController,
                     hintText: "add a location",
                   ),
                   TextFieldWidget(
-                    maxLines: 8,
-                    height: 40,
+                    maxLines: 6,
+                    //height: 40,
                     title: "Description/Instructions",
                     controller: descriptionController,
                   ),
                   TextFieldWidget(
                     maxLines: 1,
-                    height: 40,
+                    height: 38,
+                    hintText: "add a link",
                     title: "Event Link",
                     controller: linkController,
                   ),
@@ -170,46 +177,78 @@ class _EventWidgetState extends State<EventWidget> {
                         fontWeight: FontWeight.w600,
                         fontSize: 12),
                   ),
-                  Row(
-                    children: [
-                      ColorStyle(color: Color(0xFFF57272)),
-                      SizedBox(
-                        width: 5,
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      width: screenSize.width / 3,
+                      //color: Colors.red,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ColorStyle(color: Color(0xFFF57272)),
+                          ColorStyle(color: Color(0xFFE9D700)),
+                          ColorStyle(color: Color(0xFF33BA61)),
+                          ColorStyle(color: Color(0xFFF484F0)),
+                          ColorStyle(color: Color(0xFF85D7FA)),
+                          ColorStyle(color: Color(0xFF16339D)),
+                          ColorStyle(color: Color(0xFF10411B)),
+                          ColorStyle(color: Color(0xFF947445)),
+                          ColorStyle(color: Color(0xFF662970)),
+                          // ColorStyle(color: Colors.purpleAccent),
+                          // ColorStyle(color: Colors.pinkAccent),
+                        ],
                       ),
-                      ColorStyle(color: Color(0xFFE9D700)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      ColorStyle(color: Color(0xFF33BA61)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      ColorStyle(color: Color(0xFFF484F0)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      ColorStyle(color: Color(0xFF85D7FA)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      ColorStyle(color: Color(0xFF16339D)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      ColorStyle(color: Color(0xFF10411B)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      ColorStyle(color: Color(0xFF947445)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      ColorStyle(color: Color(0xFF662970)),
-                    ],
-                  )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
                 ],
               ),
             ),
+            Container(
+              height: 0.7,
+              color: Colors.grey,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 12.0, top: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          child: const Text(
+                            "Cancel",
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 13),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const CustomButton(height: 34, width: 80, text: "Next")
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            )
           ],
         ),
       ),
@@ -219,14 +258,14 @@ class _EventWidgetState extends State<EventWidget> {
 
 class TextFieldWidget extends StatefulWidget {
   final int maxLines;
-  final double height;
+  final double? height;
   final String title;
   final TextEditingController controller;
   final bool obscureText;
   final String? hintText;
   const TextFieldWidget(
       {super.key,
-      required this.height,
+      this.height,
       required this.title,
       required this.controller,
       this.obscureText = false,
@@ -253,7 +292,7 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
               fontSize: 12),
         ),
         SizedBox(
-          height: 10,
+          height: 6,
         ),
         Container(
           height: widget.height,
@@ -265,6 +304,11 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
                 fillColor: Colors.white,
                 filled: true,
                 hintText: widget.hintText,
+                hintStyle: TextStyle(
+                    color: Color(0xFF787878).withOpacity(0.7),
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 13),
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                     borderSide: BorderSide(
@@ -273,7 +317,7 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
               )),
         ),
         SizedBox(
-          height: 10,
+          height: 20,
         )
       ],
     );
